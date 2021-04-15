c5308881f0ba67273ca101b5cb6352a89a6ed297
biroghan <biroghlan95@gmail.com>
2021-04-15 23:14:41 +0430

    Create project top_book and app book and build models.

---
+++ top_book/book/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=150)
+    store_name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(default='',upload_to='store_image/',null=True)
+    fav = models.BooleanField(default=False)
+    create_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
