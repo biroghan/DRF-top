06282f3e72e360154cde1d23205027d63e622c21
biroghan <biroghlan95@gmail.com>
2021-04-19 12:46:37 +0430

    updated static_root

--- top_book/top_book/settings.py
+++ top_book/top_book/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,17 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT= BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'store_image')
 MEDIA_ROOT= '/media/'
-MEDIA_ROOT=BASE_DIR / 'store_image'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
