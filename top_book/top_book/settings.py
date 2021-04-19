565eb483483d058e83f778d50443893167cdeccd
biroghan <biroghlan95@gmail.com>
2021-04-19 11:59:04 +0430

    added djangorestframework==3.11.2 in requirements

--- top_book/top_book/settings.py
+++ top_book/top_book/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xvu5m9$1m=s3)5q$7nc2=er_ip&74qj4*3gqr(p&^g3rjumj&z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
